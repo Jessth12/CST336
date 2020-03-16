fcb9ebf0bf24924c3e1060c24f487da6c8c3e38b
EC2 Default User <ec2-user@ip-172-31-89-129.ec2.internal>
2020-03-16 07:00:25 +0000

    updated installs

--- public/assignments/assignment3/js/app.js
+++ public/assignments/assignment3/js/app.js
@@ -12,21 +12,36 @@ $("#form").submit(function(e){
 
 function queryPokeAPI(api, value){
 	$("#text").empty();
-	var url = "https://pokeapi.co/" + api + "/" + value;
+	var url = "https://pokeapi.co/api/v2/" + api + "/" + value;
 	$.ajax({
 		url: url,
 		type: "GET",
 		dataType: "json",
 		success: function(data){
 			console.log(data);
-			data.message.forEach(function(src){
-				$("#text").append();
-			});
+			if (api == "pokemon") {
+				displayPokemon(data);
+			} else {
+				displayPokemonList(data);
+			}
 		},
 		error: function(err){
 			console.log(err);
 		}
 	});
+}
+
+function displayPokemon(data) {
+	$("#text").append("<h2> " + data["name"] + " </h2>");
+	$("#text").append("<img src=" + data["sprites"]["front_default"] + "> ");
+	$("#text").append("<span> Weight in Hectograms: " + data["weight"] + " </span>");
+	
+}
+
+function displayPokemonList(data) {
+	for (let i = 0; i < data["pokemon_species"].length; i++) {
+		$("#text").append("<span> " + data["pokemon_species"][i]["name"] + " </span>");
+	}
 }
 
 

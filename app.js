6c359846c8b208d7d214b739af463010955176c9
Yessica Torrez Hernandez <jessh2182@gmail.com>
2020-03-29 21:42:59 -0700

    changes to lab7

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var lab7Router = require('./routes/lab7');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+app.set("view engine", "ejs");
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/labs/lab7', lab7Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

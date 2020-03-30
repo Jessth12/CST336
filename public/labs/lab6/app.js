d1f437203eedcc778821d02da71775cc01ffa4fe
EC2 Default User <ec2-user@ip-172-31-89-129.ec2.internal>
2020-03-30 04:02:52 +0000

    lab7

--- public/labs/lab6/app.js
+++ public/labs/lab6/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
 
 //creating routes
 app.get("/", function(req,res){
-    res.sent("it works!");
+    res.render("index.html");
 });
 
 //server listener 
-app.listen("8081", );
+app.listen("8081", "127.0.0.1", function(){
+    console.log("Express server is Running...");
+});
+
+app.get("/mercury", function(req,res){
+    res.send("This will be Mercury page!");
+});
+
+app.get("/venus", function(req,res){
+    res.send("This will be Venus page!");
+});
+
